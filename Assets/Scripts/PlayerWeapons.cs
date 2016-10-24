ee33c6d4a1bbcc4a672c3ed1c459dc9b678264f7
n.pushnoi <n.pushnoi@corp.nekki.ru>
2016-10-24 17:42:15 +0300

    Dobavil Stvoli

---
+++ Assets/Scripts/PlayerWeapons.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerWeapons : MonoBehaviour {
+    public GameObject CrossBowPL;
+    public GameObject CannonPl;
+    private bool Crossbow = false;
+    private bool Cannon = false;
+
+
+    void Start()
+    {
+       
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (IsWeapon(other.gameObject))
+        {
+            if (other.gameObject.name == "Crossbow")
+            {
+                CrossBowPL.SetActive(true);
+                Crossbow = true;
+                CannonPl.SetActive(false);
+                Destroy(other.gameObject);
+            }
+            else if (other.gameObject.name == "Cannon")
+            {
+                CannonPl.SetActive(true);
+                Cannon = true;
+                CrossBowPL.SetActive(false);
+                Destroy(other.gameObject);
+            }
+        }
+    }
+
+    bool  IsWeapon(GameObject obj)
+    {
+        return (obj.CompareTag("Weapon"));
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Alpha1) && Crossbow)
+        {
+            CrossBowPL.SetActive(true);
+            CannonPl.SetActive(false);
+        }
+
+        if (Input.GetKey(KeyCode.Alpha2) && Cannon)
+        { 
+        CannonPl.SetActive(true);
+        CrossBowPL.SetActive(false);
+        }
+    }
+
+}
---
+++ Assets/Scripts/RotateWeapon.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateWeapon : MonoBehaviour {
+
+    private Rigidbody rb;
+    
+   // Transform player;
+
+    void Start()
+
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+	void Update ()
+    {
+        transform.Rotate(new Vector3(45, 0, 0) * Time.deltaTime);
+	}
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag ("Player"))
+    //    {
+    //        player = other.transform;
+    //        transform.SetParent(player);
+    //        gameObject.transform.position = new Vector3(other.gameObject.transform.position.x, other.gameObject.transform.position.y, 0);
+                
+                
+    //    }
+    //}
+
+
+}
