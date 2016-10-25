6a43abd1015280e80b4a31b24602ca8834408d86
n.pushnoi <n.pushnoi@corp.nekki.ru>
2016-10-25 18:56:21 +0300

    Next Update

---
+++ Assets/Scripts/CrossbowShot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrossbowShot : MonoBehaviour {
+    public GameObject bolt;
+    public Transform Shot;
+    public Rigidbody rb;
+    public bool ShotRedy = true;
+
+	void Start () {
+      //  rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        if (Input.GetButton("Fire1") && (ShotRedy == true))
+        {
+            Rigidbody rbshot;
+           // bolt.gameObject.SetActive(false);
+            rbshot = Instantiate(rb, bolt.transform.position, bolt.transform.rotation) as Rigidbody;
+            rbshot.AddForce(Vector3.forward * 5000);
+        //    ShotRedy = false;
+                
+        }
+
+	}
+}
